b568b2cf4945e4911507fd082a4afd7dcd28552b
Filipe de Oliveira Ataíde <filipeoliveirataide@gmail.com>
2022-04-28 11:31:44 -0300

    Footer

--- themes/bootstrap/footer.php
+++ themes/bootstrap/footer.php
@@ -1,5 +1,63 @@
 <footer>
-    </footer>
-    < ?php wp_footer(); ?>
-  </body>
+  <footer class="pt-3">
+    <div class="footer1">
+      <div class="container py-5">
+        <h6 class="text-center">
+          <strong>
+            EXPEDIENTE
+          </strong>
+        </h6>
+        <div class="row pt-3">
+          <div class="col text-center">
+            <small>
+              Mauro Romero Leal Passos (RJ)
+              <br>
+              <b>Idealização, pesquisa e texto</b>
+            </small>
+          </div>
+          <div class="col text-center">
+            <small>
+              Maria Luiza Bazzo (SC)
+              Ricardo de Souza Carvalho (RJ)
+              Simoni Mariano Pinheiro (RJ)
+              <br>
+              <b>Colaboração na pesquisa</b>
+            </small>
+          </div>
+          <div class="col text-center">
+            <small>
+              Fabíola Gonçalves (RN)
+              José Eleutério Júnior (CE)
+              <br>
+              <b>Revisão</b>
+            </small>
+          </div>
+          <div class="col text-center">
+            <small>
+              Anderson Gomes do
+              Nascimento (RN)
+              <br>
+              <b>Arte</b>
+            </small>
+          </div>
+          <div class="col text-center">
+            <small>
+              Anderson Gomes do Nascimento (RN)
+              Mauricio da Silva Oliveira Jr. (RN)
+              Mariana de Araújo Figueiredo (RN)
+              <br>
+              <b>Programação</b>
+            </small>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="d-flex justify-content-center pb-3">
+      <img src=<?php echo get_template_directory_uri() . "/imgs/footer.png" ?> class="img-fluid" alt="">
+    </div>
+  </footer>
+</footer>
+
+</body>
+
 </html>
